c6ca888c25eeeefb4fa72a1522b2482a052fff5b
TrifonovAD <trifonov76@gmail.com>
2023-10-26 18:32:18 +0300

    Add homework 2

--- src/seminars/first/Shop/ShopTest.java
+++ src/seminars/first/Shop/ShopTest.java
@@ -1,5 +1,13 @@
 package seminars.first.Shop;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ShopTest {
 
     /*
---
+++ src/seminars/second/VehicleTest.java
@@ -0,0 +1,73 @@
+package seminars.second;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class VehicleTest {
+
+    Car car = new Car("Renault", "Logan", 2020);
+    Motorcycle moto = new Motorcycle("Suzuki", "Bandit", 2015);
+
+    //- проверка того, что экземпляр объекта Car также является экземпляром транспортного средства; (instanceof)
+    @Test
+    void instanceOf() {
+        assertThat(car instanceof Vehicle);
+    }
+
+    //- проверка того, объект Car создается с 4-мя колесами
+    @Test
+    void WheelsCar() {
+        assertThat(car.getNumWheels()).isEqualTo(4);
+    }
+
+    // - проверка того, объект Motorcycle создается с 2-мя колесами
+    @Test
+    void MotorcycleWheels() {
+        assertThat(moto.getNumWheels()).isEqualTo(2);
+    }
+
+    //- проверка того, объект Car развивает скорость 60 в режиме тестового вождения (testDrive())
+    @Test
+    void testDriveCar() {
+        car.testDrive();
+        assertThat(car.getSpeed()).isEqualTo(60);
+    }
+
+    // - проверка того, объект Motorcycle развивает скорость 75 в режиме тестового вождения (testDrive())
+    @Test
+    void testDriveMotorcycle() {
+        moto.testDrive();
+        assertThat(moto.getSpeed()).isEqualTo(75);
+    }
+
+    //- проверить, что в режиме парковки (сначала testDrive, потом park,
+    // т.е эмуляция движения транспорта) машина останавливается (speed = 0)
+    @Test
+    void parkCar() {
+        car.testDrive();
+        car.park();
+        assertThat(car.getSpeed()).isEqualTo(0);
+    }
+
+    //- проверить, что в режиме парковки (сначала testDrive, потом park
+    // т.е эмуляция движения транспорта) мотоцикл останавливается (speed = 0)
+    @Test
+    void parkMotorcycle() {
+        moto.testDrive();
+        moto.park();
+        assertThat(moto.getSpeed()).isEqualTo(0);
+    }
+}
